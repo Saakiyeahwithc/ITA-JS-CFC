a16ca24d2769b32019c88e7908003efc5918f36c
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-04 13:34:30 +0545

    Day-4.1

---
+++ Day-4/Day-4.js
@@ -0,0 +1,38 @@
+//call back function
+//that type of function jun chai arko function ma as argument vayera jancha
+/*syntax:getSomething(function(){
+
+})
+
+getSomething(()=>{
+    })*/
+
+/*Higher order function(HOF)
+foreach 
+map 
+reduce
+filter {array ko lagi matra kam garcha sabei hof le}*/
+
+//const fruits = ['mango', 'banana', 'apple']
+
+// fruits.map(function (fruit) {
+//     console.log(fruit)
+// })
+
+// fruits.forEach(function(eat){
+//     console.log(eat)
+// }) 
+
+//fruits.reduce(function(){})
+
+// fruits.filter(function(){})
+
+
+
+// const nums = [1, 2, 3, 4, 5]
+// const sqnum = []
+// nums.forEach(function (sq) {
+//     sqnum.push(sq**2)
+// })
+// console.log(sqnum)
+
