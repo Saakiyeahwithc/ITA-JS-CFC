68b60a6131dea8ef73a7e18ead42c2ffe95c2245
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-05 13:42:49 +0545

    Day-5.0.1

---
+++ Day-5/Day-5.js
@@ -0,0 +1,32 @@
+//Reduce (higher order function)
+
+// const num = [1, 2, 3, 4, 5]  
+// const sum = num.reduce((acc,num)=>
+//     {
+//         return num + acc
+//     },2)
+// console.log(sum)
+
+
+//add to cart feature in daraz using reduce
+// const products = [
+//     {
+//         product: 'A',
+//         price: 200,
+//         qty: 2
+//     },
+//     {
+//         product: 'B',
+//         price: 300,
+//         qty: 3
+//     }, {
+//         product: 'C',
+//         price: 500,
+//         qty: 7
+//     }
+// ]
+
+// const cart = products.reduce((acc, add) => {
+//     return add.price * add.qty + acc
+// }, 0)
+// console.log(cart)
